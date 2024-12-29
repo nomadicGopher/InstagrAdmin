879c12c768b60d244ecceb10ce667f52a80e3b65
Jon Vojtush <11096910+JonVojtush@users.noreply.github.com>
2024-12-29 13:58:27 -0800

    formatting, improvemnts, debugging

--- main.go
+++ main.go
@@ -12,73 +12,73 @@ import (
 )
 
 var (
-    userName *string = flag.String("username", "", "YOUR_IG_USERNAME")
-    accessToken *string = flag.String("access_token", "", "YOUR_IG_USER_ACCESS_TOKEN")
-    outDir *string = flag.String("outDir", "", "Output directory of log file.")
-    includeVerified *bool = flag.Bool("includeVerified", false, "Boolean to include verified accounts in report.")
-    now time.Time = time.Now()
+	userName        *string   = flag.String("username", "", "YOUR_IG_USERNAME")
+	accessToken     *string   = flag.String("access_token", "", "YOUR_IG_USER_ACCESS_TOKEN")
+	outDir          *string   = flag.String("outDir", "", "Output directory of log file.")
+	includeVerified *bool     = flag.Bool("includeVerified", false, "Boolean to include verified accounts in report.")
+	now             time.Time = time.Now()
 )
 
 const baseURL = "https://graph.instagram.com/"
 
 func main() {
-    flag.Parse();
+	flag.Parse()
 
-    logFile, err := os.Create(*outDir + "UnmutualConnections_" + now.Format("2006-01-02_15-04-05") + ".log")
-    if err != nil {
-        log.Println("Error creating log file:", err)
-    }
-    defer logFile.Close()
+	logFile, err := os.Create(*outDir + "UnmutualConnections_" + now.Format("2006-01-02_15-04-05") + ".log")
+	if err != nil {
+		log.Println("Error creating log file:", err)
+	}
+	defer logFile.Close()
 
-    multiWriter := io.MultiWriter(os.Stdout, logFile)
-    log.SetOutput(multiWriter)
-    log.Println("Include verified accounts in report: ", *includeVerified)
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(multiWriter)
+	log.Println("Include verified accounts in report: ", *includeVerified)
 
-    // Get user data
-    userProfileURL := fmt.Sprintf("%s?fields=id&access_token=%s&username=%s", baseURL, *accessToken, *userName)
-    userProfileResponse, err := http.Get(userProfileURL)
-    if err != nil {
-        log.Fatalln("Error fetching ", *userName, "'s ID:", err)
-    }
-    defer userProfileResponse.Body.Close()
+	// Get user data
+	userProfileURL := fmt.Sprintf("%s?fields=id&access_token=%s&username=%s", baseURL, *accessToken, *userName)
+	userProfileResponse, err := http.Get(userProfileURL)
+	if err != nil {
+		log.Fatalln("Error fetching ", *userName, "'s ID:", err)
+	}
+	defer userProfileResponse.Body.Close()
 
-    // Parse userID
-    userData := map[string]interface{}{}
-    err = json.NewDecoder(userProfileResponse.Body).Decode(&userData)
-    if err != nil {
-        log.Fatalln("Error parsing ", *userName, "'s data:", err)
-    }
-    userID := userData["id"].(string)
-        
-    // Get user's following list
-    usersFollowingDataURL := fmt.Sprintf("%s%s/following?access_token=%s", baseURL, userID, *accessToken)
-    usersFollowingDataResponse, err := http.Get(usersFollowingDataURL)
-    if err != nil {
-        log.Fatalln("Error fetching ", *userName, "'s following list:", err)
-    }
-    defer usersFollowingDataResponse.Body.Close()
+	// Parse userID
+	userData := map[string]interface{}{}
+	err = json.NewDecoder(userProfileResponse.Body).Decode(&userData)
+	if err != nil {
+		log.Fatalln("Error parsing ", *userName, "'s data:", err)
+	}
+	userID := userData["id"].(string)
 
-    // Parse following list
-    usersFollowingData := map[string]interface{}{}
-    err = json.NewDecoder(usersFollowingDataResponse.Body).Decode(&usersFollowingData)
-    if err != nil {
-        log.Fatalf("Error parsing %s's following list data: %v\n", *userName, err)
-    }
-    usersFollowingList := usersFollowingData["data"].([]interface{})
+	// Get user's following list
+	usersFollowingDataURL := fmt.Sprintf("%s%s/following?access_token=%s", baseURL, userID, *accessToken)
+	usersFollowingDataResponse, err := http.Get(usersFollowingDataURL)
+	if err != nil {
+		log.Fatalln("Error fetching ", *userName, "'s following list:", err)
+	}
+	defer usersFollowingDataResponse.Body.Close()
 
-    // Check if user follows back
+	// Parse following list
+	usersFollowingData := map[string]interface{}{}
+	err = json.NewDecoder(usersFollowingDataResponse.Body).Decode(&usersFollowingData)
+	if err != nil {
+		log.Fatalf("Error parsing %s's following list data: %v\n", *userName, err)
+	}
+	usersFollowingList := usersFollowingData["data"].([]interface{})
+
+	// Check if user follows back
 	for _, followee := range usersFollowingList {
-        followsBack := false
-        followeesInfo := followee.(map[string]interface{})
+		followsBack := false
+		followeesInfo := followee.(map[string]interface{})
 		followeesID := followeesInfo["id"].(string)
 		followeesUserName := followeesInfo["username"].(string)
-        followeesFullName := followeesInfo["full_name"].(string)
-        followeesAccountStatus := followeesInfo["account_status"].(map[string]interface{})
-        followeeIsVerified := followeesAccountStatus["is_verified"].(bool)
+		followeesFullName := followeesInfo["full_name"].(string)
+		followeesAccountStatus := followeesInfo["account_status"].(map[string]interface{})
+		followeeIsVerified := followeesAccountStatus["is_verified"].(bool)
 
-        if *includeVerified == false && followeeIsVerified {
-            continue
-        }
+		if !*includeVerified && followeeIsVerified {
+			continue
+		}
 
 		// Get followee's following list
 		followeesFollowingDataURL := fmt.Sprintf("%s%s/following?access_token=%s", baseURL, followeesID, *accessToken)
@@ -100,14 +100,14 @@ func main() {
 
 		// Check if userID is included in the list of followee's following list
 		for _, followeesFollowee := range followeesFollowingList {
-            followeesFolloweeInfo := followeesFollowee.(map[string]interface{})
+			followeesFolloweeInfo := followeesFollowee.(map[string]interface{})
 			if followeesFolloweeInfo["id"].(string) == userID {
 				followsBack = true
 			}
 		}
 
-        if followsBack != true {
-            log.Println(followeesUserName, " ( ", followeesFullName, " ) does not follow ", userID, " back.")
-        }
+		if !followsBack {
+			log.Println(followeesUserName, " ( ", followeesFullName, " ) does not follow ", userID, " back.")
+		}
 	}
 }
--- run.sh
+++ run.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# Check if the executable exists
-if [ -f "InstagrAdmin" ]; then
-  # Run the executable with parameters
-  ./InstagrAdmin -username="$(cat username)" -access_token="$(cat access_token)"
+# Function to check if the executable exists in either format
+check_executable() {
+    local exe_name=$1
+    # Check for Linux (Debian) and Windows formats
+    if [ -f "$exe_name" ] || [ -f "${exe_name}.exe" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Function to build the executable if it does not exist
+build_executable() {
+    go build main.go
+}
+
+if check_executable "InstagrAdmin"; then
+    # Run the executable with parameters
+    ./InstagrAdmin -username="$(cat username)" -access_token="$(cat access_token)"
 else
-  # Build the executable first
-  go build main.go
-  # Run the executable with parameters
-  ./InstagrAdmin -username="$(cat username)" -access_token="$(cat access_token)"
+    # Build the executable first if it does not exist
+    build_executable
+    # Run the executable with parameters after building
+    ./InstagrAdmin -username="$(cat username)" -access_token="$(cat access_token)"
 fi
