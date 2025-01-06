a64307f27516a95bc7ca3e91efb30a6780fe9283
Jon Vojtush <11096910+JonVojtush@users.noreply.github.com>
2025-01-05 23:16:41 -0700

    better error checing with a dynamic approach for updating the config values

--- main.go
+++ main.go
@@ -12,11 +12,10 @@ import (
 )
 
 var (
-	userName        *string   = flag.String("username", "", "Your Instagram user handle. *Required")
-	accessToken     *string   = flag.String("access_token", "", "Your Instagram user access token. *Required")
-	outDir          *string   = flag.String("outDir", "", "Output directory path for your report.")
-	includeVerified *bool     = flag.Bool("includeVerified", false, "Boolean to include verified accounts in report.")
-	now             time.Time = time.Now()
+	userName        = flag.String("username", "", "Your Instagram user handle. *Required")
+	accessToken     = flag.String("access_token", "", "Your Instagram user access token. *Required")
+	outDir          = flag.String("outDir", "", "Output directory path for your report.")
+	includeVerified = flag.Bool("includeVerified", false, "Boolean to include verified accounts in report.")
 )
 
 const baseURL = "https://graph.instagram.com/"
@@ -28,55 +27,47 @@ type Config struct {
 	IncludeVerified bool   `json:"includeVerified"`
 }
 
-func config() {
-	file, err := os.Open("config.json")
-	if err != nil {
-		log.Println("No config file was found; command line arguments will be used.")
-
-		// Verify required arguments are met
-		if *userName == "" {
-			log.Fatalln("username commannd line argument must not be empty.")
-		}
-
-		if *accessToken == "" {
-			log.Fatalln("access_token commannd line argument must not be empty.")
-		}
-
-		return
-	}
-	defer file.Close()
-
-	log.Println("A config file was found; key/values that are specified will override command line arguments & defaults.")
-
-	var config Config
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&config)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
+func checkRequiredConfigs(config Config, method string) {
 	if config.Username == "" {
-		log.Fatalln("username config file value cannot be empty. Please set to your Instagram user handle.")
-	} else if config.Username != "" {
-		*userName = config.Username
+		log.Fatalf("username %s must not be empty.\n", method)
 	}
 
 	if config.AccessToken == "" {
-		log.Fatalln("access_token config file value cannot be empty. Please set to your Instagram user access token.")
-	} else if config.AccessToken != "" {
-		*accessToken = config.AccessToken
-	}
-
-	if config.OutDir != "" {
-		*outDir = config.OutDir
-	}
-
-	if config.IncludeVerified {
-		*includeVerified = true
+		log.Fatalf("access_token %s must not be empty.\n", method)
 	}
 }
 
-func fetchData(userName string, accessToken string) ([]byte, error) {
+func loadConfig() Config {
+	config := Config{
+		Username:        *userName,
+		AccessToken:     *accessToken,
+		OutDir:          *outDir,
+		IncludeVerified: *includeVerified,
+	}
+
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		log.Println("No config file was found; command line arguments will be used.")
+
+		checkRequiredConfigs(config, "commannd line argument")
+
+		return config
+	}
+	defer configFile.Close()
+
+	log.Println("A config file was found; key/values that are specified will override command line arguments & defaults.")
+
+	decoder := json.NewDecoder(configFile)
+	if err := decoder.Decode(&config); err != nil {
+		log.Fatalln("Error decoding config file: ", err)
+	}
+
+	checkRequiredConfigs(config, "config file value")
+
+	return config
+}
+
+func fetchData(userName, accessToken string) ([]byte, error) {
 	url := fmt.Sprintf("%s%s?access_token=%s", baseURL, userName, accessToken)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -89,16 +80,14 @@ func fetchData(userName string, accessToken string) ([]byte, error) {
 		return nil, fmt.Errorf("error reading response body: %v", err)
 	}
 
-	// Log the raw response for debugging
-	log.Printf("Raw response for user %s: %s", userName, string(body))
-
+	log.Printf("Raw response for user %s: %s", userName, string(body)) //! DEBUG
 	return body, nil
 }
 
 func main() {
 	flag.Parse()
 
-	logFile, err := os.Create(*outDir + "UnmutualConnections_" + now.Format("2006-01-02_15-04-05") + ".log")
+	logFile, err := os.Create(*outDir + "UnmutualConnections_" + time.Now().Format("2006-01-02_15-04-05") + ".log")
 	if err != nil {
 		log.Println("Error creating log file:", err)
 	}
@@ -107,24 +96,23 @@ func main() {
 	multiWriter := io.MultiWriter(os.Stdout, logFile)
 	log.SetOutput(multiWriter)
 
-	config()
+	config := loadConfig()
 
 	not := ""
-	if !*includeVerified {
+	if !config.IncludeVerified {
 		not = " not"
 	}
 	log.Printf("Verified accounts are%s included.", not)
 
 	// Fetch data for the user
-	rawData, err := fetchData(*userName, *accessToken)
+	data, err := fetchData(config.Username, config.AccessToken)
 	if err != nil {
-		log.Fatalf("Error fetching data for user %s: %v", *userName, err)
+		log.Fatalf("Error fetching data for user %s: %v", config.Username, err)
 	}
 
-	// Parse the data (assuming JSON format)
 	var formattedData map[string]interface{}
-	if err := json.Unmarshal(rawData, &formattedData); err != nil {
-		log.Fatalf("Error parsing %s's data: %v", *userName, err)
+	if err := json.Unmarshal(data, &formattedData); err != nil {
+		log.Fatalf("Error parsing %s's data: %v", config.Username, err)
 	}
 
 	// Process the result...
